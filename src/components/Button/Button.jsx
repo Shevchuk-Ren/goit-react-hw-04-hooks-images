a377dff791c9a1eef55cb1346a65ebae483d42ff
Shevchuk-Ren <shevchuk_re@ukr.net>
2021-09-21 15:11:58 +0300

    change name button scroll

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -3,13 +3,13 @@ import { animateScroll as scroll } from 'react-scroll';
 import { LoadMoreButton } from './Button.styled';
 
 export default function Button({ pages }) {
-  const scroll = () => {
+  const scrollToBtn = () => {
     pages();
     scroll.scrollToBottom();
   };
 
   return (
-    <LoadMoreButton type="button" onClick={scroll}>
+    <LoadMoreButton type="button" onClick={scrollToBtn}>
       Load More
     </LoadMoreButton>
   );
